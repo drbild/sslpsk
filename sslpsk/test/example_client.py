98ea3f424fc4c321517d73fea21f1a95712803a9
Joerg Steffens <joerg.steffens@bareos.com>
2018-10-31 19:32:12 +0100

    fixes example code
    The server code did contain two typos.
    Also, in my environment, I had to specifiy the ciphers to use.
    Reuse listen socket to speed up testing.
    
    Also added the client and server example as files,
    to speed up testing.

---
+++ sslpsk/test/example_client.py
@@ -0,0 +1,30 @@
+import socket
+import ssl
+import sslpsk
+
+PSKS = {'server1' : 'abcdef',
+        'server2' : 'uvwxyz'}
+
+def client(host, port, psk):
+    tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp_socket.connect((host, port))
+
+    ssl_sock = sslpsk.wrap_socket(tcp_socket,
+                                  ssl_version=ssl.PROTOCOL_TLSv1,
+                                  ciphers='ALL:!ADH:!LOW:!EXP:!MD5:@STRENGTH',
+                                  psk=lambda hint: (PSKS[hint], 'client1'))
+
+    ssl_sock.sendall("ping")
+    msg = ssl_sock.recv(4)
+    print 'Client received: %s'%(msg)
+
+    ssl_sock.shutdown(socket.SHUT_RDWR)
+    ssl_sock.close()
+
+def main():
+    host = '127.0.0.1'
+    port = 6000
+    client(host, port, PSKS)
+
+if __name__ == '__main__':
+    main()
